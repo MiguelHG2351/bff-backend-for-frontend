fd0320ecfef45fd91591268f481f896d4e2b144c
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-09-15 16:43:36 -0600

    db no error

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGOBD_URI, {
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_URI}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
     
